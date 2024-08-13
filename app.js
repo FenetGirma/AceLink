e92f2cc371755b1f3a9a1f61ce4b5a96859b4c97
FenetGirma <fenutigist@gmail.com>
2024-08-13 23:05:10 +0300

    AC-32 Study Group

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const dotenv = require('dotenv');
+const studyGroupRoutes = require('./routes/sgRoutes')
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Routes
 app.use('/user', userRoutes);
+app.use('/study-groups', studyGroupRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
